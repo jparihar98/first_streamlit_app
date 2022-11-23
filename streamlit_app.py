f37209ed55bdac36f1a90fa7ba4bb5b2b18e1502
Jatinder Singh Parihar <118877448+jparihar98@users.noreply.github.com>
2022-11-23 12:10:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit as sl
 
-sl.title("My Parent's New Healthy Diner")
-sl.header("Breakfast Menu")
-sl.text("items")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
